73b1649a60ecf76a0bc71f1887b14f553d8e3732
Serpil Eyuboglu <sguleyuboglu74@gmail.com>
2022-12-08 22:01:56 +0000

    Wrote the code for First Class

--- src/day0/First.java
+++ src/day0/First.java
@@ -5,5 +5,8 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println("First my project");
+
+        System.out.println("Java is cool programming language");
+
     }
 }
